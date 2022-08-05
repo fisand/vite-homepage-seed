3152696d873941c9cfde3c8d3051e8cd4196b846
zouhang <1102639563@qq.com>
2022-08-05 16:04:12 +0800

    chore: update typing

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,3 +7,17 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '~icons/*' {
+  import type { DefineComponent } from 'vue'
+
+  const icon: DefineComponent<{}, {}, any>
+  export default icon
+}
+
+declare module '~icons/test-icons/*' {
+  import type { DefineComponent } from 'vue'
+
+  const icon: DefineComponent<{}, {}, any>
+  export default icon
+}
