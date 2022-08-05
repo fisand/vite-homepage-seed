ff5832600dccbf273ec79670ca0fd1280ea0466b
zouhang <1102639563@qq.com>
2022-08-05 14:54:24 +0800

    chore: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -28,13 +28,15 @@
                 </a-button>
               </div>
               <div class="mt-3 sm:mt-0 sm:ml-3">
-                <a-button
-                  size="large"
-                  type="secondary"
-                  class="w-full h-12 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                >
-                  Live demo
-                </a-button>
+                <router-link to="#">
+                  <a-button
+                    size="large"
+                    type="secondary"
+                    class="w-full h-12 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                  >
+                    Live demo
+                  </a-button>
+                </router-link>
               </div>
             </div>
           </div>
